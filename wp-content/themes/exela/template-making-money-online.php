f4568d1cd60bfc0dd2002f3ae324129a2f0cd4be
Paul Buys <paul.buys1@gmail.com>
2020-01-17 03:15:35 +0200

    page3

--- wp-content/themes/exela/template-making-money-online.php
+++ wp-content/themes/exela/template-making-money-online.php
@@ -19,4 +19,43 @@ get_header();
     </div>
 </div>
 
+<!--3-panel-card-lrg-left-exela-->
+
+<div class="main-container-3-panel-card-lrg-left-exela">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/page-3/image-1.jpg" alt="" class="image-3-panel-card-lrg-left-exela">
+    <div class="card-container-3-panel-card-lrg-left-exela">
+        <h1 class="custom-font">So you want to make money online?</h1>
+        <p class="custom-p-font">Despite the frequency of times people search for ways to make money online and the number of websites and pages dedicated to this topic, it is still a new question with an evolving answer.</p>
+        <p class="custom-p-font">When I worked at IBM twenty years ago the answer would have been around either having an eCommerce website or saving your corporation money with a pro-curement or self-service portal. Those answers could still be true.</p>
+        <p class="custom-p-font">A third answer has emerged in recent years. Creating a platform. Before we examine each of these answers in detail let's take a minute to examine the history of making money online and see if there are any clues.</p>
+        <p class="custom-p-font">The internet was invented in the 1960's and carried its first message in 1969. Twenty years later the world wide web invented. So the infrastructure technology has been around for about 50 years and the web itself for 30 years.</p>
+
+       </div>
+</div>
+
+<!--block-1-exela--> 
+
+<div class="main-container-block-1-exela">
+    <h1 class="custom-font white-text align-center">How do you make money online?</h1>
+    <p class="custom-p-font white-text align-center">To put the question "How do I make money online" we are only referring to the last 30 years. Let's have a look at the worlds most successful companies by Market Capacitation since 1989</p>
+    <p class="custom-p-font white-text align-center">For the vast majority of the last 30 years the biggest companies have been traditional. Petrochem, Pharma, Tele-com, Consumer Goods. These companies have made money online buy implementing computer system to auto-mate their internal processes. Companies invested millions in big back office finance, HR and operational systems. The cost saving was largely in head count. Another benefit companies has been the collection of, and access to, more data to aid better planning.</p>
+    <p class="custom-p-font white-text align-center">In the past few years Apple, Google Microsoft, Amazon and Facebook have risen to the top of the pile. Apple is a consumer electronic company. Apple take 73% of its revenue from consumer good and 27% from its ser-vice products, like itunes and ApplePay. While a significant number of iPhones are sold online, Apple also has 506 stores.</p>
+    <p class="custom-p-font white-text align-center">Amazon is of course a store. It sells products and ships them to customers. Microsoft, as we know sells software for computers. More recently it has returned to also selling products. Google and Facebook are platforms. They collate audiences and sell advertising space. Google and Amazon also provides infrastructure, which they rents out to other companies.</p>
+</div>
+
+<!--3-panel-card-sml-right-exela-->
+
+<div class="main-container-3-panel-card-sml-right-exela">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/page-2/image-2.jpg" alt="" class="image-3-panel-card-sml-right-exela">
+    <div class="card-container-3-panel-card-sml-right-exela">
+        <h1 class="custom-font">30 Years of the World Wide Web</h1>
+        <p class="custom-p-font">Interestingly, in the 30 years the world wide web has existed the most money has been "made" by companies using the technology to improve processes and save costs.</p>
+        <p class="custom-p-font">Internal process and/or processes that consumers use.</p>
+        <p class="custom-p-font">The big story in the 1990's was Enterprise Resource Planning solutions (ERP). In the run up to 2000 many old computer systems where replaced. One of the primary drivers for this activity was to solve the Y2K or millennium bug problem.</p>
+        <div class="button-container-3-panel-card-sml-right-exela">
+            <div class="button-2-controls-exela">Find Out More!</div>
+        </div>
+    </div>
+</div>
+
 <?php get_footer();?>
