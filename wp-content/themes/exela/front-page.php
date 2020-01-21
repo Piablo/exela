6500c18da7ab6e8bf41bf886c68a354c387e8ec3
Paul Buys <paul.buys1@gmail.com>
2020-01-21 12:33:02 +0200

    form-1

--- wp-content/themes/exela/front-page.php
+++ wp-content/themes/exela/front-page.php
@@ -51,11 +51,11 @@
         <div class="headline-container-exela-2">
             <h1 class="align-center">EXELA is transforming the way businesses use technology to grow.</h1>
         </div>
-            <div class="p-container-exela-2">
-                <p>It's our mission to help businesses and entrepreneurs to simplify the use of technology to improve processes, save time, generate leads, manage and make more sales while wowing customers with awesome customer service.</p>
-            </div>
+        <div class="p-container-exela-2">
+            <p>It's our mission to help businesses and entrepreneurs to simplify the use of technology to improve processes, save time, generate leads, manage and make more sales while wowing customers with awesome customer service.</p>
+        </div>
         </div> 
-</div>
+    </div>
 
 <div class="spacer"></div>
 
@@ -87,7 +87,7 @@
     </div>
     
 </div>
-<div class="main-container-exela-card-1">
+<div class="main-container-exela-card-3">
     <div class="grid-container-exela-card-3">
         <div class="card-outer-container-exela-card-3">
             <div class="card-inner-container-exela-card-3">
@@ -147,63 +147,6 @@
 </div>
 <div class="card-footer-exela-card-3"></div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-            <!--<div class="card-1-container-exela-card-3">
-                <div class="card-container-inner-1-exela-card-3">
-                    <div class="image-container-exela-card-3 image-1-exela-card-3"></div>
-                    <div class="text-container-exela-card-3">
-                        <p style="font-size: 17px">How does a stream of unsolicited referrals sound to you?</p>
-                        <p style="font-size: 17px">Good, right?</p>
-                        <p style="font-size: 17px">You still need to close the deals but referred prospects contacting you are usually your best leads</p>
-                        <p style="font-size: 17px">To generate the quality of loads you can expect from referrals is expensive. You either need a large number of leads which you are going to churn through or a terrific qualifying funnel thot filters out low quality leads and leaves you with the ones ready to buy.</p>
-                    </div>
-                    <div class="button-2-container-exela-card-3">
-                        <div class="button-2-controls-exela">Read More</div>
-                    </div>
-                </div>
-            </div>
-            <div class="card-1-container-exela-card-3">
-                <div class="card-container-inner-1-exela-card-3">
-                    <div class="image-container-exela-card-3 image-2-exela-card-3"></div>
-                    <div class="text-container-exela-card-3">
-                        <p style="font-size: 17px">Despite the frequency of times people search for ways to make money online and the number of websites and pages dedicated to this topic, it is still a new question with an evolving answer.</p>
-                        <p style="font-size: 17px">When I worked at IBM twenty years ago the answer would have boon around either having an eComrnerce website or saving your corporation money with a procurement or self-service portal</p>
-                        <p style="font-size: 17px">Those answers could still be true.</p>
-                    </div>
-                    <div class="button-2-container-exela-card-3">
-                        <div class="button-2-controls-exela">Read More</div>
-                    </div>
-                </div>
-            </div>
-            <div class="card-1-container-exela-card-3">
-                <div class="card-container-inner-1-exela-card-3">
-                    <div class="image-container-exela-card-3 image-3-exela-card-3"></div>
-                    <div class="text-container-exela-card-3">
-                        <p style="font-size: 17px">Its not easy setting up a new business. Where to start is often a bone of contention, as there are so many theories and philosophies about marketing, digital marketing, sales and business development.</p>
-                        <p style="font-size: 17px">What seems to be most needed out there is a trustworthy guide. So we've prepared a check-list of steps to take in o logical sequence for marketing communications processes and sales processes in business.</p>               
-                    </div>
-                    <div class="button-2-container-exela-card-3">
-                        <div class="button-2-controls-exela">Read More</div>
-                    </div>
-                </div>
-            </div>--> 
-        </div> 
-
-    </div>
-</div>
-
-
 <!--exela-banner-1-->
 
 <div class="main-container-exela-banner-1">
@@ -217,8 +160,47 @@
 <!--exela-form-1-->
 
 <div class="main-container-exela-form-1">
-    <div class="grid-2-container-exela-form-1">
-        <div class="container-1-exela-form-1">
+    <div class="grid-container-exela-form-1">
+        <div class="card-container-exela-form-1">
+            <div class="spacer"></div>
+            <div class="header-container-exela-form-1 align-center">
+                <h2>We-ve tailor-made our services to ensure you get exactly what you need</h2>
+                <div class="spacer"></div>
+                <p>We would love to help grow your business, find out how we can help you!</p>
+                <div class="spacer"></div>
+                <div class="control-container-exela-form-1 location-1-override">
+                    <div class="button-2-controls-exela">View our services</div>
+                </div>
+            </div>
+            <div class="spacer"></div>
+        </div>
+
+        <div class="card-container-exela-form-1">
+            <div class="spacer"></div>
+            <div class="header-container-exela-form-1 align-center">
+                <h2>We-ve tailor-made our services to ensure you get exactly what you need</h2>
+                <div class="spacer"></div>
+                <div class="control-container-exela-form-1 location-3-override">
+                    <input class="exela-input-controls" id="name" name="name" type="text" placeholder="Email Address">
+                </div>
+                <div class="spacer"></div>
+                <div class="control-container-exela-form-1 location-2-override">
+                    <input class="exela-input-controls" id="name" name="name" type="text" placeholder="Name & Surname">
+                </div>
+                <div class="spacer"></div>
+                <div class="control-container-exela-form-1 location-1-override">
+                    <div class="button-2-controls-exela">Submit</div>
+                </div>
+            </div>
+            <div class="spacer"></div>
+        </div>
+        
+    </div> 
+</div>
+
+<div class="spacer"></div>
+
+<!-- <div class="container-1-exela-form-1">
             <h1 class="custom-font align-center">We've tailor-made our services to ensure you get exactly what you need</h1>
             <p class="custom-p-font align-center">We would love to help grow your business, find out how we can help you!</p>
             <div class="button-container-exela-form-1">
@@ -226,18 +208,18 @@
             </div>
         </div>
         <div class="container-1-exela-form-1">
-        <h1 class="custom-font align-center">Stay up to date with our latest news!</h1>
-        <div class="input-1-container-exela-form-1">
-            <input class="exela-input-controls" id="name" name="name" type="text" placeholder="Email Address">
-        </div>
+            <h1 class="custom-font align-center">Stay up to date with our latest news!</h1>
+            <div class="input-1-container-exela-form-1">
+                <input class="exela-input-controls" id="name" name="name" type="text" placeholder="Email Address">
+            </div>
         <div class="input-2-container-exela-form-1">
-        <input class="exela-input-controls" id="name" name="name" type="text" placeholder="Name & Surname">
-        </div>
-        <div class="button-container-exela-form-1">
-            <div class="button-2-controls-exela" style="font-size: 1.2vw">Submit</div>
-        </div>
-        </div>
-    </div>
-</div>
+            <input class="exela-input-controls" id="name" name="name" type="text" placeholder="Name & Surname">
+            </div>
+            <div class="button-container-exela-form-1">
+                <div class="button-2-controls-exela" style="font-size: 1.2vw">Submit</div>
+            </div>
+        </div> -->
+    
+
 
 <?php get_footer();?>
